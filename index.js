b35b92018ebf4f3bc283d5fd3e8c970be91be2bb
Tabitha <flor.palida@yahoo.com>
2023-05-31 19:47:57 -0400

    finished assignments

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+const employee = {
+    first: "Tabitha",
+    last: "Carroll",
+};
+const address = {
+    street: {
+        line1: "111 Crazy Ave",
+        line2: "2nd Floor",
+    },
+    city: "Las Vegas",
+    state: "NV",
+    zipCode: "12345",
+};
+ 
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+const hairColor = updateEmployeeWithKeyAndValue(
+    employee,
+    "blue",
+);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+delete newEmployee[key]
+return newEmployee
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+delete employee[key]
+return employee
+};
